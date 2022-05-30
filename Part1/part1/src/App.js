38638a9f2129b7366b4aa38ee6e68d7c2d975cce
Jefferson Ribeiro <jeffgribeiro@gmail.com>
2022-05-30 17:36:41 -0300

    Commir with exercise 1.12

--- Part1/part1/src/App.js
+++ Part1/part1/src/App.js
@@ -1,41 +1,20 @@
 import { useState } from "react";
 
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
-
-const StatisticLine  = ({ statistic, text }) => <tr><td> {text}</td><td> {statistic} </td></tr>;
-
-const Statistics = ({good, neutral, bad}) => {
-
-  if ( good + neutral + bad > 0)
-   return <table>
-       <tbody>
-         <StatisticLine text={"Good: "} statistic={good} />
-         <StatisticLine text={"Neutral: "} statistic={neutral} />
-         <StatisticLine text={"Bad: "} statistic={bad} />
-         <StatisticLine text={"All: "} statistic={bad + neutral + good} />
-         <StatisticLine text={"Positive: "} statistic={good / (bad + neutral + good)} />
-         <StatisticLine text={"Average: "} statistic={(good - bad) / (bad + neutral + good)} />
-       </tbody>
-     </table>;
-
-    return <p>No feedback given</p>
-}
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = ["If it hurts, do it more often", "Adding manpower to a late software project makes it later!", "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.", "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", "Premature optimization is the root of all evil.", "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.", "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients"];
 
+  const [selected, setSelected] = useState(anecdotes.length-1);
+
+  function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
   return <div>
-      <h1>give feedback</h1>
-      <Button handleClick={() => setBad(bad + 1)} text={"Bad"}/>
-      <Button handleClick={() => setNeutral(neutral + 1)} text={"Neutral"}/>
-      <Button handleClick={() => setGood(good + 1)} text={"Good"}/>
-      <h1>statistics</h1>
-      
-      <Statistics good={good} bad={bad} neutral={neutral}/>
-      
+      {anecdotes[selected]}
+      <br/>
+      <button onClick={() => setSelected(getRandomArbitrary(0, anecdotes.length - 1))} >Next Anecdote</button>
     </div>;
 };
 
