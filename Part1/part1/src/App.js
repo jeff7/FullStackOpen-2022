7619948e4b645d5f630229210f122f48ae4294c5
Jefferson Ribeiro <jeffgribeiro@gmail.com>
2022-05-30 12:02:15 -0300

    Commit with exercise 1.8

--- Part1/part1/src/App.js
+++ Part1/part1/src/App.js
@@ -1,13 +1,26 @@
 import { useState } from "react";
 
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+
+const Label = ({ statistic, text }) => <p> {text} {statistic} </p>;
+
+const Statistics = ({good, neutral, bad}) => {
+   return <>
+      <Label text={"Good: "} statistic={good} />
+      <Label text={"Neutral: "} statistic={neutral} />
+      <Label text={"Bad: "} statistic={bad} />
+      <Label text={"All: "} statistic={bad + neutral + good} />
+      <Label text={"Positive: "} statistic={ good/ (bad + neutral + good)} />
+      <Label text={"Average: "} statistic={ (good - bad) / (bad + neutral + good)} />
+    
+    </>
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
-
-  const Label = ({ statistic, text }) => <p> {text} {statistic} </p>;
 
   return <div>
       <h1>give feedback</h1>
@@ -15,12 +28,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text={"Neutral"}/>
       <Button handleClick={() => setGood(good + 1)} text={"Good"}/>
       <h1>statistics</h1>
-      <Label text={"Good: "} statistic={good} />
-      <Label text={"Neutral: "} statistic={neutral} />
-      <Label text={"Bad: "} statistic={bad} />
-      <Label text={"All: "} statistic={bad + neutral + good} />
-      <Label text={"Positive: "} statistic={ good/ (bad + neutral + good)} />
-      <Label text={"Average: "} statistic={ (good - bad) / (bad + neutral + good)} />
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>;
 };
 
