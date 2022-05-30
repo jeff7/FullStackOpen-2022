a8dbfafdd4a808d4f284ba951e6c7f827357b499
Jefferson Ribeiro <jeffgribeiro@gmail.com>
2022-05-30 11:46:39 -0300

    Commit with the exercise 1.6

--- Part1/part1/src/App.js
+++ Part1/part1/src/App.js
@@ -1,46 +1,24 @@
+import { useState } from "react";
+
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      { name: "Fundamentals of React", exercises: 10 },
-      { name: "Using props to pass data", exercises: 7 },
-      { name: "State of a component", exercises: 14 }
-    ]
-  };
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const Header = () =>
-    <h1>
-      {course.name}
-    </h1>;
+  const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
 
-  const Part = props =>
-    <p>
-      {props.part} {props.exercises}
-    </p>;
+  const Label = ({ statistic, text }) => <p> {text} {statistic} </p>;
 
-  const Content = () => {
-    return (
-      <div>
-        {course.parts.map(p => <Part part={p.name} exercises={p.exercises} />)}
-      </div>
-    );
-  };
-
-  const Total = () =>
-    <p>
-      Number of exercises{" "}
-      {course.parts[0].exercises +
-        course.parts[1].exercises +
-        course.parts[2].exercises}
-    </p>;
-
-  return (
-    <div>
-      <Header />
-      <Content />
-      <Total />
-    </div>
-  );
+  return <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setBad(bad + 1)} text={"Bad"}/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"Neutral"}/>
+      <Button handleClick={() => setGood(good + 1)} text={"Good"}/>
+      <h1>statistics</h1>
+      <Label text={"Good: "} statistic={good} />
+      <Label text={"Neutral: "} statistic={neutral} />
+      <Label text={"Bad: "} statistic={bad} />
+    </div>;
 };
 
 export default App;
