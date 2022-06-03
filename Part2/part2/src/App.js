0557b48b91d4301a33676ee1f8c1429f20cc95b5
Jefferson Ribeiro <jeffgribeiro@gmail.com>
2022-06-03 14:54:18 -0300

    Commit with exercise 2.5

--- Part2/part2/src/App.js
+++ Part2/part2/src/App.js
@@ -1,36 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => {
-
-  const initialValue = 0;
-  const sumWithInitial = sum.reduce(
-    (previousValue, currentValue) => previousValue + currentValue.exercises,
-    initialValue
-  );
-
-  return <p>Number of exercises {sumWithInitial}</p>
-}
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-  {
-    parts.map(
-      (part) => <Part key={part.id} part={part} />
-    )
-  }     
-  </>
-
-const Course = ({ course }) => 
-  <>
-    <Header course={course.name}/> 
-    <Content parts={course.parts} /> 
-    <Total sum={course.parts} />
-  </>
+import Course from "./components/CourseComponent"
 
 const App = () => {
 
